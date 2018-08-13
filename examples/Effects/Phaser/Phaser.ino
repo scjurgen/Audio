8fe7df599d8e6623e24cddb5208f5f49aecb8add
Jürgen Schwietering <juergen.schwietering@teufel.de>
2018-08-13 10:39:46 +0200

    cleanup phaser

--- examples/Effects/Phaser/Phaser.ino
+++ examples/Effects/Phaser/Phaser.ino
@@ -4,111 +4,128 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+// 8 stage phaser
+
 // GUItool: begin automatically generated code
 AudioInputI2S            i2s1;           //xy=57,122
 AudioEffectDelay         delay1;         //xy=290,220
 AudioFilterBiquad        biquad1;        //xy=506,222
 AudioFilterBiquad        biquad2;        //xy=507,288
-AudioFilterBiquad        biquad3;        //xy=508,347
-AudioFilterBiquad        biquad4;        //xy=510,408
 AudioMixer4              mixer1;         //xy=683,203
 AudioOutputI2S           i2s2;           //xy=827,344
 AudioConnection          patchCord1(i2s1, 1, delay1, 0);
 AudioConnection          patchCord2(delay1, 0, mixer1, 0);
 AudioConnection          patchCord3(delay1, 1, biquad1, 0);
-AudioConnection          patchCord4(biquad1, biquad4);
-//AudioConnection          patchCord5(biquad2, biquad3);
-//AudioConnection          patchCord6(biquad3, biquad4);
-AudioConnection          patchCord7(biquad4, 0, mixer1, 1);
+AudioConnection          patchCord4(biquad1, biquad2);
+AudioConnection          patchCord7(biquad2, 0, mixer1, 1);
 AudioConnection          patchCord8(mixer1, 0, i2s2, 0);
 AudioConnection          patchCord9(mixer1, 0, i2s2, 1);
 AudioControlSGTL5000     sgtl5000_1;
 // GUItool: end automatically generated code
 
-const int myInput = AUDIO_INPUT_LINEIN;
+class Phaser
+{
+public:
+  Phaser()
+  : m_qFactor(1.0)
+  , m_f(100.0)
+  , m_direction(10.0)
+  , m_minF(200.0)
+  , m_maxF(1000.0)
+  , m_updateInMsecs(10)
+  {
+      m_nextUpdate = millis();
+  }
+  // triangle wave from minF to maxF in updateInMsecs steps
+  // obviously it would be nice to use a wave generator
+  void tick()
+  {
+      if (millis() >= m_nextUpdate)
+      {
+          m_nextUpdate = millis()+m_updateInMsecs;
+          newParams(f);
+          if (direction >= 0)
+          {
+              f += direction;
+              if (f >= m_maxF)
+                  direction = -direction;
+          }
+          else
+          {
+              f += direction;
+              if (f <= m_minF)
+                  direction = -direction;
+          }
+      }
+  }
+
+  void setLFOFrequency(double f)
+  {
+      if (f==0)
+      {
+          direction = 0;
+          return;
+      }
+      direction = (2*(maxF-minF))(1000.0/m_updateInMsecs*f);
+  }
+
+private:
+  void newParams(float new_f)
+  {
+      for (uint32_t i=0; i < 4; i++)
+      {
+          AudioNoInterrupts();
+          biquad1.setAllPass(i, new_f, m_qFactor);
+          biquad2.setAllPass(i, new_f, m_qFactor);
+          AudioInterrupts();
+      }
+  }
+
+  float m_qFactor;
+  float m_f;
+  float m_direction;
+  float m_minF;
+  float m_maxF;
+  unsigned long m_updateInMsecs;
+  unsigned long m_nextUpdate;
+};
+
+
+Phaser phaser;
 
 void setup() {
     AudioNoInterrupts();
     AudioMemory(60);
 
-    // Enable the audio shield, select input, and enable output
     sgtl5000_1.enable();
-    sgtl5000_1.inputSelect(myInput);
+    sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
     sgtl5000_1.volume(0.75);
     sgtl5000_1.lineInLevel(9);
-    for (int i=0; i < 4; i++)
-    {
-        biquad1.setAllPass(i, 200, 4.0);
-        biquad2.setAllPass(i, 200, 4.0);
-        biquad3.setAllPass(i, 200, 4.0);
-        biquad4.setAllPass(i, 200, 4.0);
-    }
     delay1.delay(0,0);
     delay1.delay(1,20);
     AudioInterrupts();
+    phaser.tick();
+    Serial.println("all set");
 }
 
-elapsedMillis volmsec=0;
-
-unsigned long nextT;
-unsigned long nextAnalysis;
-
-void newParams(float new_f)
-{
-    for (uint32_t i=0; i < 4; i++)
-    {
-        AudioNoInterrupts();
-        biquad1.setAllPass(i, new_f, 1.0);
-        AudioInterrupts();
-        AudioNoInterrupts();
-        biquad2.setAllPass(i, new_f+30+i*4, 1.0);
-        AudioInterrupts();
-        AudioNoInterrupts();
-        biquad3.setAllPass(i, new_f+10+i*8, 1.0);
-        AudioInterrupts();
-        AudioNoInterrupts();
-        biquad4.setAllPass(i, new_f+20+i*7, 1.0);
-        AudioInterrupts();
-    }
-}
-
-int f = 100;
-int direction = 10;
 void loop() {
+    phaser.tick();
 
-    if (millis() > nextT)
-    {
-        nextT = millis()+10;
-        newParams(f);
-        if (direction > 0)
-        {
-            f += direction;
-            if (f > 1000)
-                direction = -direction;
-        }
-        else
-        {
-            f += direction;
-            if (f < 200)
-                direction = -direction;
-        }
-    }
+    static unsigned long nextAnalysis = 0;
     if (millis()  >= nextAnalysis)
     {
         nextAnalysis += 10000;
-        Serial.print("all=");
+        Serial.print("AudioProcessorUsage: ");
         Serial.print(AudioProcessorUsage());
-        Serial.print(",");
+        Serial.print(" (max=");
         Serial.print(AudioProcessorUsageMax());
-        Serial.print("    ");
-        Serial.print("Memory: ");
+        Serial.print("(    ");
+        Serial.print("AudioMemoryUsage: ");
         Serial.print(AudioMemoryUsage());
-        Serial.print(",");
+        Serial.print(", (max=");
         Serial.print(AudioMemoryUsageMax());
-        Serial.println();
+        Serial.println(")");
         AudioProcessorUsageMaxReset();
     }
 }
-
-
 
--- examples/Effects/Phaser/effects_info.h
+++ examples/Effects/Phaser/effects_info.h
@@ -1,13 +1,16 @@
 /*
 PHASER effect
-  The Phaser used an Allpass biquad filter that will change
-  the phase depending on the centerfrequency.  
-  The order of the filter determines the strength.
+  The Phaser uses an allpass filter that will change
+  the phase of the signal depending on the center frequency and q-factor.
+
+  The number of stages determines the strength. Typically are 4 8 12 stage phasers
   The center frequency is continuesly shifted.
 
-  THis example does not use regen, which means to feedback 
-  the shifted signal to the entrance (with a second mixed).
+  THis example does not use have a "regen", which feeds the processed signal back
+  to the entrance (with a second mixed).
  
   Best use this effect with chords and pads.
 
-*/
+ see also: https://en.wikipedia.org/wiki/Phaser_(effect)
+
+ */
